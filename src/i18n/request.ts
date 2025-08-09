b5c5fb3f50b4a5a89533f0f521280607a3905950
syednuruzzaman <syed.nuruzzaman@yahoo.com>
2025-08-09 12:50:27 -0400

    Fix i18n message path and add generateStaticParams for locale routes

--- src/i18n/request.ts
+++ src/i18n/request.ts
@@ -10,6 +10,6 @@ export default getRequestConfig(async ({locale}) => {
 
   return {
     locale: locale as string,
-    messages: (await import(`../../messages/${locale}.json`)).default
+    messages: (await import(`../messages/${locale}.json`)).default
   };
 });
